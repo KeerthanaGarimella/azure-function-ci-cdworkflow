70b401df043a9e992bd33873ec9921e8b7c99997
KeerthanaGarimella <kgarimella5513@conestogac.on.ca>
2025-08-01 10:29:34 -0400

    Initial commit - Azure Function App with CI/CD pipeline

---
+++ HelloFunction.Tests/HelloFunctionTest.cs
@@ -0,0 +1,14 @@
+
+using Xunit;
+
+namespace HelloFunction.Tests
+{
+    public class HelloFunctionTest
+    {
+        [Fact]
+        public void Test1()
+        {
+            Assert.True(true);
+        }
+    }
+}
---
+++ HelloFunction/HelloFunction.cs
@@ -0,0 +1,22 @@
+
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using System.Threading.Tasks;
+
+namespace HelloFunctionApp
+{
+    public static class HelloFunction
+    {
+        [FunctionName("HelloFunction")]
+        public static async Task<IActionResult> Run(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = null)] HttpRequest req,
+            ILogger log)
+        {
+            log.LogInformation("C# HTTP trigger function processed a request.");
+            return new OkObjectResult("Hello, world!");
+        }
+    }
+}
